2d588b7299cab02cd0a0edeceab2c9dbbf51422f
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-24 12:42:33 -0500

    created property header file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 # include <iostream>
 # include "raylib.h"
 # include "constants.h"
+# include "property.h"
 
 using namespace std;
 
 # define print(x) cout << x << endl;
 
 int main(){
+	// INITIALIZE SCREEN
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Get Off My Lawn (Prototype)");
 
+	// GAME LOOP
 	while(!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
---
+++ property.h
@@ -0,0 +1,19 @@
+# ifndef PROPERTY
+# define PROPERTY
+
+// Define Property
+struct SQUARE {
+	int x;
+	int y;
+	int width;
+	int height;
+};
+
+struct ROOF {
+	Vector2 position;
+	int sides;
+	int size;
+	float rotation
+};
+
+# endif
