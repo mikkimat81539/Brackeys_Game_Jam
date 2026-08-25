1308985b03892350ce63949c75fbcfa3d53bc77a
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-25 17:03:41 -0500

    applied movement to player

--- main.cpp
+++ main.cpp
@@ -18,13 +18,40 @@ int main(){
 	player.y = SCREEN_HEIGHT/2;
 	player.width = 20;
 	player.height = 20;	
+	player.velocity = {500, 500};
 
-	int playerCenterX = player.x - player.width;
-	int playerCenterY = player.y - player.height;
-
+	// FPS
+	SetTargetFPS(FPS);
 
 	// GAME LOOP
 	while(!WindowShouldClose()){
+		// DELTA TIME
+		float dt = GetFrameTime();
+
+		// PLAYER MOVEMENT
+		if (IsKeyDown(KEY_LEFT)){
+			player_move_left(player, dt);
+
+		}
+
+		if (IsKeyDown(KEY_RIGHT)){
+			player_move_right(player, dt);
+
+		}
+
+		if (IsKeyDown(KEY_UP)){
+			player_move_up(player, dt);
+
+		}
+
+		if (IsKeyDown(KEY_DOWN)){
+			player_move_down(player, dt);
+
+		}
+
+		int playerCenterX = player.x - player.width;
+		int playerCenterY = player.y - player.height;
+
 
 		// DRAWING
 		BeginDrawing();
--- player.h
+++ player.h
@@ -3,11 +3,29 @@
 
 // DEFINE PLAYER
 struct Player {
-	int x;
-	int y;
-	int width;
-	int height;
-	int velocity;
+	float x;
+	float y;
+	float width;
+	float height;
+	Vector2 velocity;
 };
+
+// PLAYER MOVEMENT FUNCTIONS
+void player_move_left(Player &player, float dt){
+	player.x -= player.velocity.x * dt;
+}
+
+void player_move_right(Player &player, float dt){
+	player.x += player.velocity.x * dt;
+}
+
+void player_move_up(Player &player, float dt){
+	player.y -= player.velocity.y * dt;
+}
+
+void player_move_down(Player &player, float dt){
+	player.y += player.velocity.y * dt;
+}
+
 
 # endif
