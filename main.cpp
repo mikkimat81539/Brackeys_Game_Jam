3101cb8c822e771d77955a9129a6f66e995524d4
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-25 16:31:03 -0500

    displayed player onto screen

--- main.cpp
+++ main.cpp
@@ -2,21 +2,37 @@
 
 # include "raylib.h"
 # include "constants.h"
+# include "player.h"
 
 using namespace std;
 
 # define print(x); cout << x << endl;
 
-
 int main(){
+	// SCREEN
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Spawning Opponents");
 
-	while(!WindowShouldClose()){
-	BeginDrawing();
-	ClearBackground(RAYWHITE);
-	
-	EndDrawing();
+	// PLAYER
+	Player player;
+	player.x = SCREEN_WIDTH/2;
+	player.y = SCREEN_HEIGHT/2;
+	player.width = 20;
+	player.height = 20;	
 
+	int playerCenterX = player.x - player.width;
+	int playerCenterY = player.y - player.height;
+
+
+	// GAME LOOP
+	while(!WindowShouldClose()){
+
+		// DRAWING
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawRectangle(playerCenterX, playerCenterY, player.width, player.height, BLACK);
+		
+		EndDrawing();
 	}
 
 	CloseWindow();
---
+++ player.h
@@ -0,0 +1,13 @@
+# ifndef PLAYER
+# define PLAYER
+
+// DEFINE PLAYER
+struct Player {
+	int x;
+	int y;
+	int width;
+	int height;
+	int velocity;
+};
+
+# endif
