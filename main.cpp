02da6e94c8e63f2fcb3d7a4e42161ca561ee3f39
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-26 12:36:46 -0500

    displayed player

--- main.cpp
+++ main.cpp
@@ -14,6 +14,16 @@ int main(){
 	// SCREEN
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Collision Practice");
 
+	// PLAYER
+	Player player;
+	player.position.x = SCREEN_CENTER.x;
+	player.position.y = SCREEN_CENTER.y;	
+	player.width = 25;
+	player.height = 50;
+	
+	float playerCenterX = player.position.x - player.width;
+	float playerCenterY = player.position.y - player.height;
+
 	// FPS
 	SetTargetFPS(FPS);
 
@@ -21,6 +31,8 @@ int main(){
 	while(!WindowShouldClose()){
 		BeginDrawing();	
 		ClearBackground(RAYWHITE);		
+
+		DrawRectangle(playerCenterX, playerCenterY, player.width, player.height, BLACK);
 
 		EndDrawing();
 	}
