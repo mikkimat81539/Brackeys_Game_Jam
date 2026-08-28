a0311a42d8446dc2e2ab91c6abafdf5e6b6fa4ba
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-28 16:25:20 -0500

    displayed idle right sprite onto screen

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
 
 # include "raylib.h"
 # include "constants.h"
-//# include "player.h"
-//# include "projectile.h"
-//# include "opponents.h"
+# include "mechanics.h"
 
 using namespace std;
 
@@ -24,16 +22,41 @@ int main() {
 	Texture2D front_fence = LoadTexture("assets/front_fence.png");
 	Texture2D back_fence = LoadTexture("assets/back_fence.png");
 
+	// PLAYER
+	Texture2D idle_right_img = LoadTexture("assets/idle_right.png");
+
+	Player idle_right;
+	idle_right.position = {350, 350}; // position of sprite
+	idle_right.frameRec = {0, 0, 128, 128}; // crop spritesheet 
+	idle_right.direction = IDLE_RIGHT;
+
+	int frameCount = 0; // number of frames in spritesheet
+
 	// FPS
 	SetTargetFPS(FPS);
 
 	// GAME LOOP
 	while(!WindowShouldClose()){
+		float dt = GetFrameTime();
+	
+		// IDLE RIGHT FRAME	
+		if (idle_right.direction == IDLE_RIGHT){
+			frameCount++; // iterate through frames
+
+			if (frameCount >= 28) {
+				frameCount = 0;
+			}
+		}
+		
 
 
 		// DRAW
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+
+		if (idle_right.direction == IDLE_RIGHT){
+			DrawTextureRec(idle_right_img, idle_right.frameRec, idle_right.position, WHITE);
+		}
 
 		DrawTexture(house, 0, 0, WHITE);
 		DrawTexture(front_fence, 0, 470, WHITE);	
---
+++ mechanics.h
@@ -0,0 +1,21 @@
+# ifndef MECHANICS
+# define MECHANICS
+
+enum STATE {
+	IDLE_RIGHT,
+	IDLE_LEFT,
+	RUN_RIGHT,
+	RUN_LEFT,
+	UP,
+	DOWN
+};
+
+struct Player {
+	Vector2 position; // position of the sprite
+	Rectangle frameRec; // put the sprite in a rect
+	Vector2 velocity;
+
+	STATE direction;
+};
+
+# endif
