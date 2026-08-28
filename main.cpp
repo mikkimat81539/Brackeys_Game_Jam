c5d27f8f1465aee9b49fa3f8e9beabbbffc2964c
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-27 22:47:10 -0500

    added player texture onto screen

--- main.cpp
+++ main.cpp
@@ -19,21 +19,32 @@ int main() {
 	Texture2D front_fence = LoadTexture("assets/front_fence.png");
 	Texture2D back_fence = LoadTexture("assets/back_fence.png");
 
+	// PLAYER
+	Texture2D player = LoadTexture("assets/idle.png");
+
+	Sprite sprite;
+	sprite.position = {SCREEN_CENTER};
+	sprite.frameRec = {0, 0, 128, 128};
+	
 	// FPS
 	SetTargetFPS(FPS);
 
 	// GAME LOOP
 	while(!WindowShouldClose()){
 
+
+
+
 		// DRAW
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
+		ClearBackground(WHITE);
 		
-		DrawTexture(house, 0, 0, WHITE);
+		DrawTexture(house, 0, 0, WHITE); // house
 
-		DrawTexture(front_fence, 0, 470, WHITE);
-		DrawTexture(back_fence, 0, 250, WHITE);
+		DrawTexture(front_fence, 0, 470, WHITE); // front fence
+		DrawTexture(back_fence, 0, 250, WHITE); // back fence
 
+		DrawTextureRec(player, sprite.frameRec, sprite.position, WHITE); // player
 
 		EndDrawing();
 	}
