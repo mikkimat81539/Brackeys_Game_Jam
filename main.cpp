ae43550b470bc7884218c5ae656e4f52c00c9310
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-28 15:50:25 -0500

    added house to display

--- main.cpp
+++ main.cpp
@@ -16,15 +16,26 @@ using namespace std;
 int main() {
 	// SCREEN
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Get Off My Lawn");
+	
+	// HOUSE
+	Texture2D house = LoadTexture("assets/house.png");
 
+	// FENCE
+	Texture2D front_fence = LoadTexture("assets/front_fence.png");
+	Texture2D back_fence = LoadTexture("assets/back_fence.png");
 
 	// FPS
 	SetTargetFPS(FPS);
 
 	// GAME LOOP
 	while(!WindowShouldClose()){
+
+
+		// DRAW
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+
+		DrawTexture(house, 0, 0, WHITE);
 
 		EndDrawing();
 	}
