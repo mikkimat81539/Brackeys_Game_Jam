90863685825a11bbcd6933b8ecb50a8565c58965
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-27 22:34:36 -0500

    created the scenery

--- main.cpp
+++ main.cpp
@@ -12,13 +12,30 @@ int main() {
 	// SCREEN
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Practice");
 
+	// HOUSE
+	Texture2D house = LoadTexture("assets/house.png");
+
+	// FENCE
+	Texture2D front_fence = LoadTexture("assets/front_fence.png");
+	Texture2D back_fence = LoadTexture("assets/back_fence.png");
+
+	// FPS
+	SetTargetFPS(FPS);
+
 	// GAME LOOP
 	while(!WindowShouldClose()){
+
+		// DRAW
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		
+		DrawTexture(house, 0, 0, WHITE);
+
+		DrawTexture(front_fence, 0, 470, WHITE);
+		DrawTexture(back_fence, 0, 250, WHITE);
+
 
 		EndDrawing();
-
 	}
 
 	CloseWindow();
