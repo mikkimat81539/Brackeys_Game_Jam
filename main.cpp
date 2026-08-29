f1a8cb7bf64f5df2d6260f243eb2fa867b4ede0e
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-28 19:53:06 -0500

    added running sprite

--- main.cpp
+++ main.cpp
@@ -41,6 +41,23 @@ int main() {
 	idle_left.frameRec = {0, 0, 128, 128}; // crop spritesheet 
 	idle_left.velocity = {100, 100};
 
+	// run right
+	Texture2D run_right_img = LoadTexture("assets/run_right.png");
+
+	Player run_right;
+	run_right.position = idle_right.position; // position of sprite
+	run_right.frameRec = {0, 0, 128, 128}; // crop spritesheet 
+	run_right.velocity = {100, 100};
+	
+
+	// run left
+	Texture2D run_left_img = LoadTexture("assets/run_left.png");
+
+	Player run_left;
+	run_left.position = idle_left.position; // position of sprite
+	run_left.frameRec = {0, 0, 128, 128}; // crop spritesheet 
+	run_left.velocity = {100, 100};
+
 
 	// frame rate data
 	int frameCount = 0; // number of frames in spritesheet
@@ -58,50 +75,87 @@ int main() {
 
 		// KEY INPUT
 		if (IsKeyDown(KEY_RIGHT)) {
-			player_state = IDLE_RIGHT;
-		}
-
-		if (IsKeyDown(KEY_LEFT)) {
-			player_state = IDLE_LEFT;
-		}
-
-
-		// IDLE RIGHT
-		if (player_state == IDLE_RIGHT){
+			player_state = RUN_RIGHT;
 
 			// FRAME RATE
 			frameTimer++;
 
-			if (frameTimer >= 8){
+			if (frameTimer >= 6){
 				frameTimer = 0;
 				frameCount++; // iterate through frames
 
-				if (frameCount >= 28) {
+				if (frameCount >= 34) {
 					frameCount = 0;
 				}
 				
-				idle_right.frameRec.x = frameCount * idle_right.frameRec.width;
-				idle_right.frameRec.y = frameCount * idle_right.frameRec.height;
+				run_right.frameRec.x = frameCount * run_right.frameRec.width;
+				run_right.frameRec.y = frameCount * run_right.frameRec.height;
 			}
 
 		}
 
-		// IDLE FRAME LEFT
-		if (player_state == IDLE_LEFT){
+
+		if (IsKeyDown(KEY_LEFT)) {
+			player_state = RUN_LEFT;
+
+			// FRAME RATE
 			frameTimer++;
 
-			if (frameTimer >= 8){
+			if (frameTimer >= 6){
 				frameTimer = 0;
 				frameCount++; // iterate through frames
 
-				if (frameCount >= 28) {
+				if (frameCount >= 34) {
 					frameCount = 0;
 				}
 				
-				idle_left.frameRec.x = frameCount * idle_left.frameRec.width;
-				idle_left.frameRec.y = frameCount * idle_left.frameRec.height;
+				run_left.frameRec.x = frameCount * run_left.frameRec.width;
+				run_left.frameRec.y = frameCount * run_left.frameRec.height;
+			}
+		}
+
+		
+		else {
+			// IDLE RIGHT
+			if (player_state == IDLE_RIGHT){
+
+				// FRAME RATE
+				frameTimer++;
+
+				if (frameTimer >= 8){
+					frameTimer = 0;
+					frameCount++; // iterate through frames
+
+					if (frameCount >= 28) {
+						frameCount = 0;
+					}
+					
+					idle_right.frameRec.x = frameCount * idle_right.frameRec.width;
+					idle_right.frameRec.y = frameCount * idle_right.frameRec.height;
+				}
+
+				idle_right.position.x = run_right.position.x;
 			}
 
+			// IDLE FRAME LEFT
+			if (player_state == IDLE_LEFT){
+				frameTimer++;
+
+				if (frameTimer >= 8){
+					frameTimer = 0;
+					frameCount++; // iterate through frames
+
+					if (frameCount >= 28) {
+						frameCount = 0;
+					}
+					
+					idle_left.frameRec.x = frameCount * idle_left.frameRec.width;
+					idle_left.frameRec.y = frameCount * idle_left.frameRec.height;
+				}
+
+				idle_left.position.x = run_left.position.x;
+
+			}
 		}
 
 		// MOVING UP AND DOWN LEFT STATE
@@ -113,8 +167,20 @@ int main() {
 			idle_right.position.y += idle_right.velocity.y * dt;
 		}
 
+
+		if (IsKeyDown(KEY_RIGHT)){
+			run_right.position.x += run_right.velocity.x * dt;
+
+		}
+
+		else if (IsKeyDown(KEY_LEFT)){
+			run_left.position.x -= run_left.velocity.x * dt;
+
+		}
+
 		// Keep left-facing sprite at the exact same position
 		idle_left.position = idle_right.position;
+
 
 
 		// BOUNDARIES
@@ -141,6 +207,14 @@ int main() {
 
 		if (player_state == IDLE_LEFT){
 			DrawTextureRec(idle_left_img, idle_left.frameRec, idle_left.position, WHITE);
+		}
+
+		if (player_state == RUN_RIGHT){
+			DrawTextureRec(run_right_img, run_right.frameRec, run_right.position, WHITE);
+		}
+
+		if (player_state == RUN_LEFT){
+			DrawTextureRec(run_left_img, run_left.frameRec, run_left.position, WHITE);
 		}
 
 		DrawTexture(house, 0, 0, WHITE);
